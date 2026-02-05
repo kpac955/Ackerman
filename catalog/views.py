08b113696f3eeaa2ea6b28783f2db7daf4cee3eb
kotbk <kpas95545@gmail.com>
2026-02-06 01:03:10 +0400

    Final changes to homework#22

---
+++ catalog/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+
+# Главная страница
+def home(request):
+    return render(request, 'catalog/index.html')
+
+
+# Функция для страницы контактов
+def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+
+        print(f'Новая заявка: {name}, Почта: {email}, Текст: {message}')
+
+    return render(request, 'catalog/contacts.html')
+
+
+from django.shortcuts import render
+
+# Create your views here.
