5b0ced99aac4316136ff15570ca035529f186544
kotbk <kpas95545@gmail.com>
2026-02-14 00:54:07 +0400

    Закончена работа над домашкой 22

--- catalog/models.py
+++ catalog/models.py
@@ -1,1 +1,37 @@
-# Create your models here.
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Наименование")
+    description = models.TextField(verbose_name="Описание", blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=150, verbose_name="Наименование")
+    description = models.TextField(verbose_name="Описание")
+    image = models.ImageField(
+        upload_to="products/", verbose_name="Изображение", blank=True, null=True
+    )
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, verbose_name="Категория"
+    )
+    price = models.DecimalField(
+        max_digits=10, decimal_places=2, verbose_name="Цена за покупку"
+    )
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата изменения")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+        ordering = ("name",)
--- catalog/views.py
+++ catalog/views.py
@@ -1,18 +1,27 @@
 from django.shortcuts import render
 
+from .models import Product
 
-# Главная страница
+
 def home(request):
-    return render(request, "catalog/index.html")
+    # Берем все товары из базы данных
+    products = Product.objects.all()
+
+    # Вывод 5 последних товаров
+    latest_products = Product.objects.order_by("-created_at")[:5]
+    print("Последние 5 товаров:")
+    for p in latest_products:
+        print(f"ID: {p.id}, Название: {p.name}")
+
+    context = {
+        "object_list": products,
+    }
+    return render(request, "catalog/index.html", context)
 
 
-# Функция для страницы контактов
 def contacts(request):
     if request.method == "POST":
         name = request.POST.get("name")
-        email = request.POST.get("email")
         message = request.POST.get("message")
-
-        print(f"Новая заявка: {name}, Почта: {email}, Текст: {message}")
-
+        print(f"Имя: {name}, Сообщение: {message}")
     return render(request, "catalog/contacts.html")
