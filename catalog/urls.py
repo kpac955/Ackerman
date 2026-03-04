b16eea44adaa83ae88ad001195719efec2a073bd
kotbk <kpas95545@gmail.com>
2026-03-05 00:41:58 +0400

    final home_work_26.1

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from catalog.views import ProductListView, ProductDetailView, ContactTemplateView
+from catalog.views import (ProductListView, ProductDetailView, ContactTemplateView,
+                           ProductCreateView, ProductUpdateView, ProductDeleteView)
 
 app_name = 'catalog'
 
@@ -7,4 +8,7 @@ urlpatterns = [
     path('', ProductListView.as_view(), name='home'),
     path('contacts/', ContactTemplateView.as_view(), name='contacts'),
     path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('product/create/', ProductCreateView.as_view(), name='product_create'),
+    path('product/<int:pk>/update/', ProductUpdateView.as_view(), name='product_update'),
+    path('product/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,7 @@
-from django.views.generic import ListView, DetailView, TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, TemplateView, CreateView, UpdateView, DeleteView
 from catalog.models import Product
+from catalog.forms import ProductForm
 
 class ProductListView(ListView):
     model = Product
@@ -11,3 +13,20 @@ class ProductDetailView(DetailView):
 
 class ContactTemplateView(TemplateView):
     template_name = 'catalog/contacts.html'
+
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'catalog/product_form.html'
+    success_url = reverse_lazy('catalog:home')
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'catalog/product_form.html'
+    success_url = reverse_lazy('catalog:home')
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    template_name = 'catalog/product_confirm_delete.html'
+    success_url = reverse_lazy('catalog:home')
